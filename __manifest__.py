23da202ba22b9a791346ea386f2954e503408fc5
Ibnu Subhan <ibnusubhanisacz27@gmail.com>
2026-07-07 02:05:51 +0700

    Initialize Odoo 19 module structure

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Candidate Interview Management",
+    "version": "19.0.1.0.0",
+    "summary": "Candidate Interview Management Module",
+    "description": """Manage candidate interview process.""",
+    "author": "Ibnu Subhan",
+    "category": "Human Resources",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "data": [],
+    "installable": True,
+    "application": True,
+}
